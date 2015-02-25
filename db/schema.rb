63c258aa9dd8fd3bb290e5247de5d72969602e41
mstanden91 <mstanden91@gmail.com>
2015-02-25 15:38:20 -0300

    eliminar kind de formulario y tabla y controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224143425) do
+ActiveRecord::Schema.define(version: 20150225183512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20150224143425) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "kind"
     t.string   "name"
     t.string   "age_range_from"
     t.string   "age_range_to"
